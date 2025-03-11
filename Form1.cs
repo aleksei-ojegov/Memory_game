da69d0f8f2de511a94fa8ee7817a60cb46a7f2e2
aleksei-ojegov <aleksei.ojegov@yandex.ru>
2025-03-11 10:59:26 +0400

    1
    first

---
+++ Form1.cs
@@ -0,0 +1,172 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Memory_3
+{
+    public partial class Form1 : Form
+    {
+        Random random = new Random();
+        string[,] data = new string[16, 2];
+        string[] proverka = new string[16];
+        string[] baza = new string[16];
+
+        PictureBox firstClicked = null;
+        PictureBox secondClicked = null;
+
+        string firstClick = null;
+        string secondClick = null;
+
+        int ff = 0;
+
+        List<string> icons = new List<string>()
+        {
+            "im1", "im1", "im2", "im2", "im3", "im3", "im4", "im4",
+            "im5", "im5", "im6", "im6", "im7", "im7", "im8", "im8"
+        };
+
+        public Form1()
+        {
+            InitializeComponent();
+            AssignIconsToSquares();
+        }
+
+        private void AssignIconsToSquares()
+        {
+            int n = 0;
+            foreach (Control control in tableLayoutPanel1.Controls)
+            {
+                PictureBox pictureBox = control as PictureBox;
+                if (pictureBox != null)
+                {
+                    int randomNumber = random.Next(icons.Count);
+                    pictureBox.Image = Properties.Resources.question;
+                    //pictureBox.Image = (Image)Properties.Resources.ResourceManager.GetObject(icons[randomNumber]);
+                    pictureBox.SizeMode = PictureBoxSizeMode.StretchImage;
+                    data[n, 0] = pictureBox.Name.ToString();
+                    data[n, 1] = icons[randomNumber];
+                    icons.RemoveAt(randomNumber);
+                    n++;
+                }
+            }
+
+            string ss = "";
+
+            for (int i = 0; i < 16; i++)
+            {
+                for (int j = 0; j < 2; j++)
+                {
+                    ss += data[i, j] + " ";
+                }
+                ss += "\n";
+            }
+            //MessageBox.Show(ss);
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            if (timer1.Enabled == true)
+                return;
+
+            PictureBox pictureBox = sender as PictureBox;
+            int ss = 0;
+
+            if (pictureBox != null)
+            {
+                for(int i = 0;i < 16;i++)
+                {
+                    if(data[i, 0] == pictureBox.Name.ToString())
+                    {
+                        ss = i; break;
+                    }
+                }
+
+                //if (pictureBox.Image == Properties.Resources.question)
+                for (int i = 0; i < 16; i++)
+                {
+                    if (pictureBox.Name.ToString() == baza[i])
+                    {
+                        //MessageBox.Show("Ты уже нажал");
+                        return;
+                    }
+                }
+                
+                if (firstClicked == null)
+                {
+                    firstClicked = pictureBox;
+                    firstClick = data[ss, 1];
+                    ff = ss;
+                    firstClicked.Image = (Image)Properties.Resources.ResourceManager.GetObject(data[ss, 1]);
+                    return;
+                }
+
+                secondClicked = pictureBox;
+                secondClick = data[ss, 1];
+                secondClicked.Image = (Image)Properties.Resources.ResourceManager.GetObject(data[ss, 1]);
+
+                
+
+                if (firstClick == secondClick)
+                {
+                    proverka[ss] = secondClick;
+                    proverka[ff] = firstClick;
+                    baza[ss] = firstClicked.Name.ToString();
+                    baza[ff] = secondClicked.Name.ToString();
+                    firstClicked = null;
+                    secondClicked = null;
+                    firstClick = null;
+                    secondClick = null;
+
+                    //MessageBox.Show(ff.ToString() + " " + ss.ToString());
+                    CheckForWinner();
+                    return;
+                }
+
+                
+
+                timer1.Start();
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            timer1.Stop();
+
+            firstClicked.Image = Properties.Resources.question;
+            secondClicked.Image = Properties.Resources.question;
+
+            firstClicked = null;
+            secondClicked = null;
+
+            firstClick = null;
+            secondClick = null;
+        }
+
+        private void CheckForWinner()
+        {
+            int i = 0;
+            foreach (Control control in tableLayoutPanel1.Controls)
+            {
+                PictureBox pictureBox = control as PictureBox;
+                
+                if (pictureBox != null)
+                {
+                    //if (pictureBox.Image != (Image)Properties.Resources.ResourceManager.GetObject("question"))
+                    //    return;
+                    if(proverka[i] == null)
+                        return;
+                    
+                }
+                i++;
+            }
+            MessageBox.Show("Ты нашёл все картинки!", "Победа");
+            Close();
+        }
+    }
+}
